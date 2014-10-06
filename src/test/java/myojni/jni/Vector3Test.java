de0554b9bdf140537d6f652cf80883f6faad2eea
John <john@johnluetke.net>
2014-10-05 18:44:03 -0700

    Unit test for Vector3

---
+++ src/test/java/myojni/jni/Vector3Test.java
@@ -0,0 +1,30 @@
+package myojni.jni;
+
+import org.hamcrest.CoreMatchers;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class Vector3Test {
+
+    Vector3 vector;
+
+    @Before
+    public void SetUp() {
+        Hub hub = new Hub("myojni.junit.tests");
+    }
+
+    @Test
+    public void testX() {
+        Vector3 vector = new Vector3(0.1f, 0f, 0f);
+        Assert.assertThat(vector.X(), CoreMatchers.is(0.1f));
+        //Assert.assertEquals(0.1f, vector.X(), 0);
+    }
+
+    @After
+    public void TearDown() {
+        if (vector != null)
+            vector.dispose();
+    }
+}
