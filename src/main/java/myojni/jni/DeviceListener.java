d437b33cb89644ab529bb74995096382dc939203
John <john@johnluetke.net>
2014-10-05 21:54:58 -0700

    Refactoring to interface

--- src/main/java/myojni/jni/DeviceListener.java
+++ src/main/java/myojni/jni/DeviceListener.java
@@ -1,30 +1,26 @@
 package myojni.jni;
 
-import myojni.Disposable;
+public interface DeviceListener {
 
-public abstract class DeviceListener implements Disposable {
+    public void onPair(Myo myo, long timestamp, FirmwareVersion firmwareVersion);
 
-    public abstract void onPair(Myo myo, long timestamp, FirmwareVersion firmwareVersion);
+    public void onUnpair(Myo myo, long timestamp);
 
-    public abstract void onUnpair(Myo myo, long timestamp);
+    public void onConnect(Myo myo, long timestamp, FirmwareVersion firmwareVersion);
 
-    public abstract void onConnect(Myo myo, long timestamp, FirmwareVersion firmwareVersion);
+    public void onDisconnect(Myo myo, long timestamp);
 
-    public abstract void onDisconnect(Myo myo, long timestamp);
+    public void onArmRecognized(Myo myo, long timestamp, Arm arm, XDirection xDirection);
 
-    public abstract void onArmRecognized(Myo myo, long timestamp, Arm arm, XDirection xDirection);
+    public void onArmLost(Myo myo, long timestamp);
 
-    public abstract void onArmLost(Myo myo, long timestamp);
+    public void onPose(Myo myo, long timestamp, Pose pose);
 
-    public abstract void onPose(Myo myo, long timestamp, Pose pose);
+    //public void onOrientationData(Myo myo, long timestamp, Quaternion rotation);
 
-    //public abstract void onOrientationData(Myo myo, long timestamp, Quaternion<float> rotation);
+    public void onAccelerometerData(Myo myo, long timestamp, Vector3 accel);
 
-    public abstract void onAccelerometerData(Myo myo, long timestamp, Vector3 accel);
+    public void onGyroscopeData(Myo myo, long timestamp, Vector3 gyro);
 
-    public abstract void onGyroscopeData(Myo myo, long timestamp, Vector3 gyro);
-
-    public abstract void onRssi(Myo myo, long timestamp, byte rssi);
-
-    public native void dispose();
+    public void onRssi(Myo myo, long timestamp, int rssi);
 }
