d7946254a01ee438d004444d135d9a3e630c52a7
John <john@johnluetke.net>
2014-10-05 18:42:25 -0700

    Updating JNI handle method names

--- src/main/cpp/myojni_jni_Hub.cpp
+++ src/main/cpp/myojni_jni_Hub.cpp
@@ -5,30 +5,31 @@ void Java_myojni_jni_Hub_initialize(JNIEnv *env, jobject obj, jstring applicatio
 	std::string appId(env->GetStringUTFChars(applicationIdentifier, 0), env->GetStringUTFLength(applicationIdentifier));
 	myo::Hub hub(appId);
     myo::Hub * pHub = & hub;
-	setHandle(env, obj, pHub);
+	setJNIHandle(env, obj, pHub);
 }
 
 jobject Java_myojni_jni_Hub_waitForMyo(JNIEnv *env, jobject obj, jlong milliseconds) {
     std::cout << ">>> " << "Java_jni_Hub_waitForMyo" << std::endl;
     std::cout << "milliseconds: " << (unsigned int)milliseconds << std::endl;
-    myo::Hub * hubPtr = getHandle<myo::Hub>(env, obj);
+    myo::Hub * hubPtr = getJNIHandle<myo::Hub>(env, obj);
     //myo::Myo * myoPtr = hubPtr->waitForMyo((unsigned int)milliseconds); // TODO: SIGSEGV
+    return NULL;
 }
 
 void Java_myojni_jni_Hub_run(JNIEnv *env, jobject obj, jlong duration_ms) {
     std::cout << ">>> " << "Java_myojni_jni_Hub_run" << std::endl;
     std::cout << "duration_ms: " << (unsigned int)duration_ms << std::endl;
-    myo::Hub * pHub = getHandle<myo::Hub>(env, obj);
+    myo::Hub * pHub = getJNIHandle<myo::Hub>(env, obj);
     //pHub->run((unsigned int)duration_ms); // TODO: SIGSEGV
 }
 
 void Java_myojni_jni_Hub_runOnce(JNIEnv *env, jobject obj, jlong duration_ms) {
     std::cout << ">>> " << "Java_myojni_jni_Hub_runOnce" << std::endl;
     std::cout << "duration_ms: " << (unsigned int)duration_ms << std::endl;
-    myo::Hub * pHub = getHandle<myo::Hub>(env, obj);
+    myo::Hub * pHub = getJNIHandle<myo::Hub>(env, obj);
     //pHub->runOnce((unsigned int)duration_ms); // TODO: SIGSEGV
 }
 
 void Java_myojni_jni_Hub_dispose(JNIEnv *env, jobject obj) {
-    clearHandle<myo::Hub>(env, obj);
+    clearJNIHandle<myo::Hub>(env, obj);
 }
