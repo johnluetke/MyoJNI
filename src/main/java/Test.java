b185cf97548d49898052fc79159c61ddded942c7
John <john@johnluetke.net>
2014-10-05 18:43:53 -0700

    Updating test class

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,13 +1,19 @@
 import myojni.jni.Hub;
+import myojni.jni.Vector3;
 
 public class Test {
 
 	public static void main(String[] args) {
-		Hub hub = new Hub("com.example.test");
-        /*Myo myo = */hub.waitForMyo(0);
-        hub.run(1000);
-        hub.runOnce(1000);
-        hub.dispose();
+		Hub hub = new Hub("com.example.test"); // Loads the native lib
+        ///*Myo myo = */hub.waitForMyo(0);
+        //hub.run(1000);
+        //hub.runOnce(1000);
+        //hub.dispose();
+
+        Vector3 v3 = new Vector3();
+        v3 = new Vector3(1f,2f,3f);
+        //System.out.println(v3.X());
+        System.out.println(v3);
 	}
 	
 }
